1f77e8a682d97ee575f56cfc67ad434dab2a2652
Mage <mage@home.localhost>
2025-05-24 08:34:54 -0400

    Remove unnessesary index.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def chart_view(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, 'dashboard/base.html')
